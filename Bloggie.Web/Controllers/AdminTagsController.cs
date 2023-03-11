9aa26c8f3c951612acdbd5dbdec63a0033c69ad6
Enes Karamanoğlu <eneskrmnoglu98@gmail.com>
2023-03-11 15:00:24 +0300

    navbar-admin-dropdown-add and addtag-controller-view-shared-created

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")] //Data annotation ile farklı bir isim verirken aynı action'ı çalıştırması gerekiyor. O yüzden bu şekilde kullanım yapıyoruz.
+        public IActionResult SubmitTag()
+        {
+            var name = Request.Form["name"]; // Formdan gelen verileri tuttuk.
+            var displayName = Request.Form["displayName"];
+
+            return View("Add");
+        }
+
+
+    }
+}
